9aec1b57a5523781a337a2fedb7a0837857a9ed7
Clay Risser <clayrisser@gmail.com>
2021-10-16 13:35:23 -0500

    Initial commit

---
+++ mkpm.mk
@@ -0,0 +1,45 @@
+# File: /mkpm.mk
+# Project: patch-operator
+# File Created: 16-10-2021 13:12:34
+# Author: Clay Risser
+# -----
+# Last Modified: 16-10-2021 13:13:46
+# Modified By: Clay Risser
+# -----
+# BitSpur Inc (c) Copyright 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MKPM_PACKAGES := \
+	gnu=0.0.1
+
+MKPM_REPOS := \
+	https://gitlab.com/bitspur/community/mkpm-stable.git
+
+############# MKPM BOOTSTRAP SCRIPT BEGIN #############
+MKPM_BOOTSTRAP := https://bitspur.gitlab.io/community/mkpm/bootstrap.mk
+NULL := /dev/null
+TRUE := true
+ifeq ($(OS),Windows_NT)
+	NULL = nul
+	TRUE = type nul
+endif
+-include .mkpm/.bootstrap.mk
+.mkpm/.bootstrap.mk:
+	@mkdir .mkpm 2>$(NULL) || $(TRUE)
+	@cd .mkpm && \
+		$(shell curl --version >$(NULL) 2>$(NULL) && \
+			echo curl -L -o || \
+			echo wget --content-on-error -O) \
+		.bootstrap.mk $(MKPM_BOOTSTRAP) >$(NULL)
+############## MKPM BOOTSTRAP SCRIPT END ##############
